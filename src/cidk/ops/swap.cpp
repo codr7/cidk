464f1ec2f49a085221604f35af2e71db8e9ad535
codr7 <andreas.gone.wild@gmail.com>
2019-06-26 23:43:33 +0200

    added swap op

---
+++ src/cidk/ops/swap.cpp
@@ -0,0 +1,14 @@
+#include "cidk/cx.hpp"
+#include "cidk/ops/swap.hpp"
+
+namespace cidk::ops {
+  const SwapType Swap("swap");
+
+  SwapType::SwapType(string id): OpType(id) { }
+
+  void SwapType::eval(Cx &cx, const Op &op) const {
+    Stack &s(cx.stack);
+    auto i(s.end());
+    iter_swap(i-1, i);
+  }
+}
---
+++ src/cidk/ops/swap.hpp
@@ -0,0 +1,22 @@
+#ifndef CIDK_OP_SWAP_HPP
+#define CIDK_OP_SWAP_HPP
+
+#include "cidk/op.hpp"
+#include "cidk/val.hpp"
+
+namespace cidk::ops {
+  struct SwapType: OpType {
+    SwapType(string id);
+    virtual void eval(Cx &cx, const Op &op) const override;
+  };
+
+  extern const SwapType Swap;
+}
+
+
+namespace cidk {
+  template <>
+  inline void init(Op &op, const ops::SwapType &type) { }
+}
+
+#endif
