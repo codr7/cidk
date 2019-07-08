2783a8468e8bede9140d6448c85c175aa8e3985c
codr7 <andreas.gone.wild@gmail.com>
2019-07-08 22:47:27 +0200

    sweep pruned vars

--- src/cidk/call.cpp
+++ src/cidk/call.cpp
@@ -25,7 +25,7 @@ namespace cidk {
       cx.eval(target.body, e, stack);
       
       if (cx.eval_state == EvalState::recall) {
-        if (opt.env_extend) { e.prune(); }
+        if (opt.env_extend) { e.prune(pos); }
         cx.eval_state = EvalState::go;
         goto recall;
       }
--- src/cidk/env.cpp
+++ src/cidk/env.cpp
@@ -69,9 +69,13 @@ namespace cidk {
     std::swap(src.items, items);
   }
 
-  void Env::prune() {
+  void Env::prune(const Pos &pos) {
     for (auto i(items.begin()); i != items.end();) {
       if (i->second->env == this) {
+        Var &v(*i->second);
+        cx.refs.erase(v.ref_it);
+        v.sweep(pos);
+        
         i = items.erase(i);
       } else {
         i++;
