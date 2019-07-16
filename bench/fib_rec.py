eb803f6cdff3a7728cf3f49ef23aca10d1702dfb
codr7 <andreas.gone.wild@gmail.com>
2019-07-17 00:57:43 +0200

    simplified benchmarks

--- bench/fib_rec.py
+++ bench/fib_rec.py
@@ -2,7 +2,8 @@ from bench import bench
 
 print(bench(10, '''
 def fib(n):
-  return n if n < 2 else fib(n-1) + fib(n-2)
+  if n < 2: return n
+  return fib(n-1) + fib(n-2)
 ''', '''
 fib(20)
 '''))
--- bench/fib_tail.py
+++ bench/fib_tail.py
@@ -2,7 +2,9 @@ from bench import bench
 
 print(bench(10, '''
 def fib(n, a, b):
-  return a if n == 0 else b if n == 1 else fib(n-1, b, a+b)
+  if n == 0: return a
+  if n == 1: return b
+  return fib(n-1, b, a+b)
 ''', '''
 for _ in range(10000):
   fib(20, 0, 1)
