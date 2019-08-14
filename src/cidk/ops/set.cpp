dde79b76015e07ad654283a57d1b09a30029311e
codr7 <andreas.gone.wild@gmail.com>
2019-08-14 04:40:47 +0200

    set args

--- src/cidk/ops/set.cpp
+++ src/cidk/ops/set.cpp
@@ -3,24 +3,17 @@
 #include "cidk/ops/set.hpp"
 #include "cidk/read.hpp"
 #include "cidk/types/expr.hpp"
+#include "cidk/types/reg.hpp"
 #include "cidk/types/sym.hpp"
 
 namespace cidk::ops {
-  struct SetData {
-    Val key, val;
-    Int reg;
-    bool is_expr;
-    
-    SetData(const Val &key, const Val &val, bool is_expr):
-      key(key), val(val), reg(-1), is_expr(is_expr) {}
-  };
-
   const SetType Set("set");
 
   SetType::SetType(const string &id): OpType(id) {}
 
   void SetType::init(Cx &cx, Op &op, const Val &key, const Val &val) const {
-    op.data = SetData(key, val, val.type->isa(cx.expr_type));
+    op.args[0] = key;
+    op.args[1] = val;
   }
 
   void SetType::compile(Cx &cx,
@@ -30,13 +23,16 @@ namespace cidk::ops {
                         Ops &out,
                         Opts &opts) const {
     auto &p(in->pos);
-    auto &d(in->as<SetData>());
-    d.val.compile(p, env, opts);
+    auto &args(in->args);
+    args[1].compile(p, env, opts);
+    auto &k(args[0]);
     
-    if (d.key.type == &cx.sym_type) {      
-      if (auto found(opts.regs.find(d.key.as_sym)); found != opts.regs.end()) {
-        d.reg = found->second;
+    if (k.type == &cx.sym_type) {      
+      if (auto found(opts.regs.find(k.as_sym)); found != opts.regs.end()) {
+        k.reset(cx.reg_type, found->second);
       }
+    } else if (k.type != &cx.int_type) {
+        throw ESys(p, "Invalid id: ", k.type->id);
     }
                  
     out.push_back(*in);
@@ -44,25 +40,23 @@ namespace cidk::ops {
 
   void SetType::eval(Cx &cx, Op &op, Env &env, Reg *regs) const {
     auto &p(op.pos);
-    auto &d(op.as<SetData>());
+    auto &args(op.args);
+    auto &k(args[0]), &v(args[1]);
 
-    if (d.is_expr) {
-      cx.push(p,
-              (d.key.type == &cx.int_type)
-              ? cx.stack[d.key.as_int]
-              : regs[d.reg]);
+    if (v.type == &cx.expr_type) {
+      cx.push(p, (k.type == &cx.int_type) ? cx.stack[k.as_int] : regs[k.as_reg]);
     }
 
-    d.val.eval(p, env, regs);
+    v.eval(p, env, regs);
   
-    if (d.key.type == &cx.int_type) {
-      cx.stack[d.key.as_int] = cx.pop(p);
+    if (k.type == &cx.int_type) {
+      cx.stack[k.as_int] = cx.pop(p);
     } else {
-      regs[d.reg] = cx.pop(p);
+      regs[k.as_reg] = cx.pop(p);
     }
   }
 
-  void SetType::mark_refs(Op &op) const { op.as<SetData>().val.mark_refs(); }
+  void SetType::mark_refs(Op &op) const { op.args[1].mark_refs(); }
 
   void SetType::read(Cx &cx, Pos &pos, istream &in, Ops &out) const {
     Pos p(pos);
@@ -71,11 +65,6 @@ namespace cidk::ops {
       auto k(read_val(cx, pos, in));
       if (!k) { throw ESys(p, "Missing ;"); }
       if (k->is_eop()) { break; }
-
-      if (k->type != &cx.int_type && k->type != &cx.sym_type) {
-        throw ESys(p, "Invalid set id: ", k->type->id);
-      }
-
       auto v(read_val(cx, pos, in));
       if (!v) { throw ESys(p, "Missing ;"); }
       out.emplace_back(cx, p, *this, *k, v->is_eop() ? cx.$ : *v);
