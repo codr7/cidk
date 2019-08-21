2673608a941f496158e47367b2a8fafa05c99d7d
codr7 <andreas.gone.wild@gmail.com>
2019-08-22 01:28:02 +0200

    use default args

--- bench/fib_tail.py
+++ bench/fib_tail.py
@@ -1,10 +1,9 @@
 from bench import bench
 
 print(bench(10, '''
-def fib(n, a, b):
+def fib(n, a=0, b=1):
   if n > 1: fib(n-1, b, a+b)
   return a if n == 0 else b
 ''', '''
-for _ in range(10000):
-  fib(20, 0, 1)
+for _ in range(10000): fib(20)
 '''))
